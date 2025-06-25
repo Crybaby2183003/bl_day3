f1a6f259feb3b337b39ea1a2d70555cc80dab5af
Himanshu Singh <142487121+Crybaby2183003@users.noreply.github.com>
2025-06-25 15:28:15 +0530

    Add files via upload

---
+++ dam.java
@@ -0,0 +1,19 @@
+public class dam {
+    
+    static int add(int a, int b){
+        int result=a+b;
+        return result;
+    }
+    static int sub(int a, int b){
+        int result=a-b;
+        return result;
+    }
+
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        System.out.println(add(a,b));
+        System.out.println(sub(a,b));
+    }
+    
+}
